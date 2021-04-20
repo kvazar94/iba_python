7301a76bb8d889b36f7a73d285bfdfeddb0bb45d
kvazar94 <54156444+kvazar94@users.noreply.github.com>
2021-04-20 18:20:16 +0300

    Update lab1_1.py

--- lab1/lab1_1.py
+++ lab1/lab1_1.py
@@ -1,1 +1,15 @@
-
+# 8. Дано натуральное четырехзначное число n.
+# Верно ли, что все его цифры различны?
+import random
+N = random.randint(1000, 9999)
+n = 4
+print("Число n:", N)
+check = []
+for d in str(N):
+    check.append(int(d))
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if check[i] == check[j]:
+            print("Число n имеет одинаковые цифры")
+            quit()
+print("Все цифры уникальны")
